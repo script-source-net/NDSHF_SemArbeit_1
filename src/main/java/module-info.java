664ae26b92a05dc6e644068db7a917235cb32462
Marco <marco.rensch@area59.ch>
2022-01-07 09:20:28 +0100

    Add files via upload

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module com.tbt.trainthebrain {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.tbt.trainthebrain to javafx.fxml;
+    exports com.tbt.trainthebrain;
+}
