add821febdb4937ddb2f3c7321a2ebe426f182e5
Marco <marco.rensch@area59.ch>
2022-01-07 15:40:25 +0100

    Styling

--- src/main/java/com/tbt/trainthebrain/QuestionsetupController.java
+++ src/main/java/com/tbt/trainthebrain/QuestionsetupController.java
@@ -1,4 +1,30 @@
 package com.tbt.trainthebrain;
 
-public class QuestionsetupController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class QuestionsetupController implements Initializable {
+    @FXML
+    VBox editListQuestionContainerOutter;
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+        System.out.println("init called");
+        HBox questionElement = new HBox();
+        questionElement.getStyleClass().add("editListQuestionContainer");
+        questionElement.setPadding(new Insets(10));
+        Text questionText = new Text("Eine einfache Frage?");
+        questionText.getStyleClass().add("editListQuestionText");
+        questionElement.getChildren().add(questionText);
+
+        editListQuestionContainerOutter.getChildren().add(questionElement);
+
+    }
 }
