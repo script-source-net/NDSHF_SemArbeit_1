8940df9bb48caa7b342eeabe48405f1f90790946
Marco <marco.rensch@area59.ch>
2022-01-07 13:17:28 +0100

    Basis views für questioning.fxml und questioningsetup.fxml erstellt main.css erstellt und die views verlinkt

--- src/main/java/com/tbt/trainthebrain/MainController.java
+++ src/main/java/com/tbt/trainthebrain/MainController.java
@@ -1,14 +1,40 @@
 package com.tbt.trainthebrain;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class MainController {
-    @FXML
-    private Label welcomeText;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    public void switchToLearningModeClick(ActionEvent actionEvent) {
+        Stage stage = (Stage) ((Node) actionEvent.getTarget()).getScene().getWindow();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("questioning.fxml"));
+        try {
+            Scene questioningScene = new Scene(loader.load());
+            QuestioningController sceneController = loader.getController();
+            stage.setScene(questioningScene);
+        }catch (IOException ioe){
+            System.out.println("Could not load scene");
+            ioe.printStackTrace();
+        }
+    }
+
+    public void switchToEditQuestionsClick(ActionEvent actionEvent) {
+        Stage stage = (Stage) ((Node) actionEvent.getTarget()).getScene().getWindow();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("questioningsetup.fxml"));
+        try {
+            Scene questioningScene = new Scene(loader.load());
+            QuestionsetupController sceneController = loader.getController();
+            stage.setScene(questioningScene);
+        }catch (IOException ioe){
+            System.out.println("Could not load scene");
+            ioe.printStackTrace();
+        }
     }
 }
