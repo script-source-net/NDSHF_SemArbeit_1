0999e0faeee76104aa637ee96ebfb951f976de08
Marco <marco.rensch@area59.ch>
2022-01-07 09:28:50 +0100

    Created Java17 Project Added Answer.java & Question.java

---
+++ src/main/java/com/tbt/trainthebrain/Question.java
@@ -0,0 +1,9 @@
+package com.tbt.trainthebrain;
+
+import java.util.ArrayList;
+
+public class Question {
+    private int id;
+    private String question;
+    private ArrayList<Answer> answers;
+}
