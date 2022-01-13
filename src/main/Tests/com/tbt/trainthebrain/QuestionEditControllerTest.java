895c2bd5466db737ff05a18d5e9dc990e1c884e8
Claudia <clodos_m92@hotmail.com>
2022-01-13 17:54:28 +0100

    Junit Dependencies geändert wegen Bug --> Keine Tests gefunden

---
+++ src/main/Tests/com/tbt/trainthebrain/QuestionEditControllerTest.java
@@ -0,0 +1,17 @@
+package com.tbt.trainthebrain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class QuestionEditControllerTest {
+
+    @Test
+
+    public void Test(){
+
+        boolean richtig = true;
+        assertTrue(richtig);
+    }
+
+}
