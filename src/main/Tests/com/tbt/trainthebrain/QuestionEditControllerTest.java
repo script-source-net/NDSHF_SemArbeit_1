b07a274d3100dc88ff133a5b6420e395652b9526
Claudia <clodos_m92@hotmail.com>
2022-01-14 21:18:47 +0100

    Leerer JunitTest

--- src/main/Tests/com/tbt/trainthebrain/QuestionEditControllerTest.java
+++ src/main/Tests/com/tbt/trainthebrain/QuestionEditControllerTest.java
@@ -3,40 +3,19 @@ package com.tbt.trainthebrain;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class QuestionEditControllerTest {
 
 
-    @Test
-    public void verifyMinOneAnswerIsTrue_shouldReturnTrue_FAKE(){
-        QuestionEditController qec = new FakeQuestionEditController();
-        boolean result = qec.verifyMinOneAnswerIsTrue();
-        Assertions.assertTrue(result);
+   @Test
+    public void test(){
+       boolean richtig = true;
+       Assertions.assertTrue(richtig);
 
-    }
-    @Test
-    public void verifyMinOneAnswerIsTrueForTestingFalse_shouldReturnFalse_FAKE(){
-        FakeQuestionEditController fqec = new FakeQuestionEditController();
-        boolean result = fqec.verifyMinOneAnswerIsTrueForTestingFalse();
-        Assertions.assertFalse(result);
-
-
-    }
-    @Test
-    public void verifyIfMinTwoAnswers_shouldReturnTrue_FAKE(){
-        FakeQuestionEditController fqec = new FakeQuestionEditController();
-        boolean result = fqec.verifyIfMinTwoAnswers();
-        Assertions.assertTrue(result);
-    }
-
-    @Test
-    public void verifyIfMinTwoAnswersForTestingFalse_shouldReturnFalse_FAKE(){
-        FakeQuestionEditController fqec = new FakeQuestionEditController();
-        boolean result = fqec.verifyIfMinTwoAnswersForTestingFalse();
-        Assertions.assertFalse(result);
-
-    }
+   }
 
 
 
