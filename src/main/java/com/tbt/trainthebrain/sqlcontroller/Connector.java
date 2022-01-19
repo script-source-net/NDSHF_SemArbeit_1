2a8b3f4ccba12bb915afbf9de3e999dd4f22ae28
Marco <marco.rensch@area59.ch>
2022-01-19 10:45:24 +0100

    Refactoring: Removed Unused method

--- src/main/java/com/tbt/trainthebrain/sqlcontroller/Connector.java
+++ src/main/java/com/tbt/trainthebrain/sqlcontroller/Connector.java
@@ -1,7 +1,6 @@
 package com.tbt.trainthebrain.sqlcontroller;
+
 import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
 
 public class Connector {
     private final String url;
@@ -10,7 +9,7 @@ public class Connector {
     private final int port;
     private final String user;
     private final String pwd;
-    private Connection connection = null;
+    private final Connection connection = null;
 
     public Connector(String url, int port, String database, String databaseType, String user, String pwd) {
         this.url = url;
@@ -28,24 +27,5 @@ public class Connector {
         this.port = SQLConnectionData.getPORT();
         this.user = SQLConnectionData.getUSER();
         this.pwd = SQLConnectionData.getPASSWORD();
-    }
-
-    public Connection getConnection(){
-
-        String address = "jdbc:"+databaseType+"://" + url;
-        if(this.port != 0){
-            address += ":"+this.port;
-        }
-        if(this.database != null && !this.database.isEmpty()){
-            address += "/"+database;
-        }
-        System.out.println(address);
-        try{
-            connection = DriverManager.getConnection(address, this.user, this.pwd);
-        }catch(SQLException sqle){
-            System.out.println("SQL Exception beim Verbindungsaufbau aufgetreten");
-            System.out.println(sqle.getMessage());
-        }
-        return connection;
     }
 }
